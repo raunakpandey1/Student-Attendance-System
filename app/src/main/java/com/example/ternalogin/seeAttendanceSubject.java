598141fdcbb7641d0550479deb1f62829aa5f4de
Sushil kumar Gupta <skgupta77159@gmail.com>
2020-10-14 15:42:42 +0530

    Fourth Commit by Sushil Kumar

---
+++ app/src/main/java/com/example/ternalogin/seeAttendanceSubject.java
@@ -0,0 +1,89 @@
+package com.example.ternalogin;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class seeAttendanceSubject extends AppCompatActivity {
+
+    Spinner spinner;
+    Button button;
+    FirebaseDatabase database;
+    DatabaseReference databaseRef;
+    List<String> SubjectList = new ArrayList<>();;
+    ArrayAdapter<String> adapter;
+    String Subject = "Graphics";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_see_attendance_subject);
+
+        database = FirebaseDatabase.getInstance();
+        databaseRef = database.getReference("subjects");
+
+        spinner = findViewById(R.id.spinnerSub);
+        button = findViewById(R.id.subject_button_see);
+
+        RetrieveData();
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Subject = SubjectList.get(position);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent MainIntent = new Intent(seeAttendanceSubject.this, Posts.class);
+                MainIntent.putExtra("sub",Subject);
+                startActivity(MainIntent);
+            }
+        });
+
+    }
+
+    private void RetrieveData() {
+
+        databaseRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.exists()){
+                    SubjectList.clear();
+                    for(DataSnapshot subSnapshot: snapshot.getChildren()){
+                        String subj = subSnapshot.getKey();
+                        SubjectList.add(subj);
+                    }
+                    adapter = new ArrayAdapter<String>(seeAttendanceSubject.this,android.R.layout.simple_spinner_dropdown_item, SubjectList);
+                    spinner.setAdapter(adapter);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
+    }
+}
