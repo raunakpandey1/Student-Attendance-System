b304b89dad645c92c9a9fa5cc12e8ad5f020e1cb
Sushil kumar Gupta <skgupta77159@gmail.com>
2021-03-24 15:38:46 +0530

    Added messagepopup

--- app/src/main/java/com/example/ternalogin/showDefaulterStudent.java
+++ app/src/main/java/com/example/ternalogin/showDefaulterStudent.java
@@ -1,14 +1,21 @@
 package com.example.ternalogin;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.ternalogin.adapter.StudentMonthAdapter;
 
@@ -37,6 +44,40 @@ public class showDefaulterStudent extends AppCompatActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(showDefaulterStudent.this));
         showDefaulterAdapter.notifyDataSetChanged();
         recyclerView.setAdapter(showDefaulterAdapter);
+
+        button_def.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final AlertDialog dialog=new AlertDialog.Builder(showDefaulterStudent.this).create();
+                View view= LayoutInflater.from(showDefaulterStudent.this).inflate(R.layout.messagepopup,null);
+                final EditText messageHere;
+                Button cancelBtn, confirmBtn;
+                messageHere = findViewById(R.id.messageHere);
+                cancelBtn=view.findViewById(R.id.canclebtn);
+                confirmBtn=view.findViewById(R.id.confirmbtn);
+                dialog.setCancelable(true);
+                dialog.setView(view);
+
+                cancelBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+                    }
+                });
+                confirmBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String Message = messageHere.getText().toString();
+                        if(TextUtils.isEmpty(Message)){
+                            Toast.makeText(showDefaulterStudent.this, "No message entered...", Toast.LENGTH_SHORT).show();
+                        }else{
+                            //
+                        }
+                    }
+                });
+            }
+        });
+
     }
 
     @Override
