d8dbcdc1525a8574b65ae6fdbb40e000e1d01e60
Sushil kumar Gupta <skgupta77159@gmail.com>
2021-04-06 13:05:47 +0530

    created toolbar menu

--- app/src/main/java/com/example/ternalogin/MainActivity.java
+++ app/src/main/java/com/example/ternalogin/MainActivity.java
@@ -7,10 +7,14 @@ import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -22,7 +26,8 @@ import com.google.firebase.database.ValueEventListener;
 public class MainActivity extends AppCompatActivity {
 
     private TextView tname,tdept,tsub,temail,TAbutton,SSbutton,addStudent,exitApp;
-    private ImageButton imgbutton1;
+    Toolbar toolbar;
+   // private ImageButton imgbutton1;
     private FirebaseAuth mAuth;
     private DatabaseReference dataRef;
     CardView cdTA,cdVA,cdAS,cdEA;
@@ -38,11 +43,12 @@ public class MainActivity extends AppCompatActivity {
         dataRef = FirebaseDatabase.getInstance().getReference("Faculty").child(CurrentUserId);
 
 
+        toolbar = findViewById(R.id.mainToolbar);
         TAbutton = findViewById(R.id.TAButton);
         SSbutton = findViewById(R.id.SSButton);
         addStudent = findViewById(R.id.add_student);
         exitApp = findViewById(R.id.exit);
-        imgbutton1 = findViewById(R.id.fac_toolbar_logout);
+       // imgbutton1 = findViewById(R.id.fac_toolbar_logout);
         tname = findViewById(R.id.teach_name);
         tdept = findViewById(R.id.teach_dept);
         tsub = findViewById(R.id.teach_sub);
@@ -52,6 +58,8 @@ public class MainActivity extends AppCompatActivity {
         cdVA = findViewById(R.id.cdVA);
         cdAS = findViewById(R.id.cdAS);
         cdEA = findViewById(R.id.cdEA);
+
+        setSupportActionBar(toolbar);
 
         dataRef.addValueEventListener(new ValueEventListener() {
             @Override
@@ -75,7 +83,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        imgbutton1.setOnClickListener(new View.OnClickListener() {
+       /* imgbutton1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 mAuth.signOut();
@@ -90,7 +98,7 @@ public class MainActivity extends AppCompatActivity {
                 overridePendingTransition(0,0);
                 finish();
             }
-        });
+        });  */
 
 
         cdTA.setOnClickListener(new View.OnClickListener() {
@@ -124,5 +132,33 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(MainIntent);
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.upDoc:
+                Toast.makeText(this, "document", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.logout:
+                mAuth.signOut();
+                Intent LoginIntent = new Intent(MainActivity.this, Login_Activity.class);
+                LoginIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                LoginIntent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                startActivity(LoginIntent);
+                overridePendingTransition(0,0);
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
     }
 }
